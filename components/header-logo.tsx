013dacfdfc8c3f0ad1aa2aa6750a245e3c8128e5
Abhishek Rohilla <157987980+rohillaabhishek@users.noreply.github.com>
2024-12-02 04:46:29 +0530

    Update header-logo.tsx

--- components/header-logo.tsx
+++ components/header-logo.tsx
@@ -7,7 +7,7 @@ export const HeaderLogo = () => {
       <div className="items-center hidden lg:flex">
         <Image src="/logo.svg" alt="Logo" height={28} width={28} />
         <p className="font-semibold text-white text-2xl ml-2.5">
-          SpendWise
+          SpendWize
         </p>
       </div>
     </Link>
