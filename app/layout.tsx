f023ec2efe23e1d28cbfcba8f46a1cdee7dc5511
Abhishek Rohilla <157987980+rohillaabhishek@users.noreply.github.com>
2024-12-02 04:47:36 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "SpendWise",
+  title: "SpendWize",
   description: "This is your Financial Overview Report",
 };
 
